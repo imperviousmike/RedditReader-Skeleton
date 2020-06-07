6090263f3f4aa82d6cc160d635f2c7a9ce365e19
Michael Daly <daly0100@algonquinlive.com>
2020-06-07 10:32:44 -0400

    logic factory initial version

--- src/main/java/logic/LogicFactory.java
+++ src/main/java/logic/LogicFactory.java
@@ -1,14 +1,36 @@
 package logic;
 
+import dal.DataAccessLayer;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  *
- * @author Shariar (Shawn) Emami
+ * @author Michael
  */
 public class LogicFactory {
 
-    //this is just a place holder to keep the code working.
-    public static AccountLogic getFor(String account) {
-        return new AccountLogic();
+    private static final String PACKAGE = "logic";
+    private static final String SUFFIX = "Logic";
+
+    public static <T extends DataAccessLayer<T>> T getFor(String entityName) {
+
+        try {
+            return getFor((Class< T>) Class.forName(PACKAGE + entityName + SUFFIX));
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
     }
-    
+
+    public static <T extends DataAccessLayer<T>> T getFor(Class<T> type) throws ClassNotFoundException {
+        try {
+            Constructor<T> declaredConstructor = type.getDeclaredConstructor();
+            return declaredConstructor.newInstance();
+
+        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+            throw new ClassNotFoundException();
+        }
+
+    }
+
 }
