d547f58ace7404a14aa7effdb7e6b4b6e3a20410
Michael Daly <daly0100@algonquinlive.com>
2020-06-06 22:32:07 -0400

    Skeleton added

---
+++ src/main/java/logic/LogicFactory.java
@@ -0,0 +1,14 @@
+package logic;
+
+/**
+ *
+ * @author Shariar (Shawn) Emami
+ */
+public class LogicFactory {
+
+    //this is just a place holder to keep the code working.
+    public static AccountLogic getFor(String account) {
+        return new AccountLogic();
+    }
+    
+}
