ae5e9b5b072e0898e08f2f2ea77583fdc89e04e6
Jonathan Cordingley <cord0059@algonquinlive.com>
2020-06-09 17:59:36 -0400

    Added createEntity().

--- src/main/java/logic/ImageLogic.java
+++ src/main/java/logic/ImageLogic.java
@@ -10,6 +10,7 @@ import dal.ImageDAL;
 import entity.Board;
 import entity.Host;
 import entity.Image;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Arrays;
@@ -27,7 +28,7 @@ import static logic.BoardLogic.URL;
  * @author jonat
  */
 public class ImageLogic extends GenericLogic<Image, ImageDAL> {
-    
+
     public static final SimpleDateFormat FORMATTER = new SimpleDateFormat("YYYY-MM-DD hh:mm:ss");
     public static final String ID = "id";
     public static final String URL = "url";
@@ -35,49 +36,50 @@ public class ImageLogic extends GenericLogic<Image, ImageDAL> {
     public static final String DATE = "date";
     public static final String LOCAL_PATH = "localPath";
     public static final String BOARD_ID = "boardId";
-    
-    ImageLogic(){
+
+    ImageLogic() {
         super(new ImageDAL());
-        
+
     }
+
     @Override
-    public List<Image> getAll(){
+    public List<Image> getAll() {
         return get(() -> dal().findAll());
     }
+
     @Override
-    public Image getWithId(int id){
+    public Image getWithId(int id) {
         return get(() -> dal().findById(id));
     }
-    
-    public List<Image> getImagesWithBoardId(int boardID){
+
+    public List<Image> getImagesWithBoardId(int boardID) {
         return get(() -> dal().findByBoardId(boardID));
     }
-    
-    public List<Image> getImagesWithTitle(String title){
+
+    public List<Image> getImagesWithTitle(String title) {
         return get(() -> dal().findByTitle(title));
     }
-    
-    public Image getImageWithUrl(String url){
+
+    public Image getImageWithUrl(String url) {
         return get(() -> dal().findByUrl(url));
     }
-    
-    public Image getImageWithLocalPath(String path){
+
+    public Image getImageWithLocalPath(String path) {
         return get(() -> dal().findByLocalPath(path));
     }
-    
-    public List<Image> getImagesWithDate(Date date){
+
+    public List<Image> getImagesWithDate(Date date) {
         return get(() -> dal().findByDate(date));
     }
-    
-    public String convertDate(Date date){
+
+    public String convertDate(Date date) {
         return FORMATTER.format(date);
     }
-    
-    @Override
-    public Image createEntity(Map<String, String[]> parameterMap){
-      
-        //do not create any logic classes in this method.
 
+    @Override
+    public Image createEntity(Map<String, String[]> parameterMap) {
+
+        //do not create any logic classes in this method.
         Objects.requireNonNull(parameterMap, "parameterMap cannot be null");
         //same as if condition below
 //        if (parameterMap == null) {
@@ -112,47 +114,46 @@ public class ImageLogic extends GenericLogic<Image, ImageDAL> {
         //converted to appropriate type. have in mind that values are
         //stored in an array of String; almost always the value is at
         //index zero unless you have used duplicated key/name somewhere.
+        String date = parameterMap.get(DATE)[0];
         String url = parameterMap.get(URL)[0];
         String title = parameterMap.get(TITLE)[0];
-        String date = parameterMap.get(DATE)[0];
         String localPath = parameterMap.get(LOCAL_PATH)[0];
-        String boardID = parameterMap.get(BOARD_ID)[0];
         
+        Date extractedDate;
+        try {
+            extractedDate = FORMATTER.parse(date);
+        } catch (ParseException ex) {
+            throw new ValidationException(ex);
+        }
         
-
         //validate the data
         validator.accept(url, 255);
         validator.accept(title, 1000);
-        validator.accept(date, 8);      //8 because it's the length of DATETIME sql datatype?
         validator.accept(localPath, 255);
-        validator.accept(boardID, );
-
+        
         //set values on entity
         entity.setUrl(url);
         entity.setTitle(title);
-        entity.setDate(date);
-        entity.setBoard(boardID);
+        entity.setDate(extractedDate);
 
         return entity;
     }
-        
-    
-    
-    public Image updateEntity(Map<String, String[]> parameterMap){
+
+    public Image updateEntity(Map<String, String[]> parameterMap) {
         
     }
-    
+
     @Override
-    public List<String> getColumnNames(){
+    public List<String> getColumnNames() {
         return Arrays.asList("ID, URL, Title, Date, LocalPath,BoardID");
     }
-    
-    public List<String> getColumnCodes(){
+
+    public List<String> getColumnCodes() {
         return Arrays.asList(ID, URL, TITLE, DATE, LOCAL_PATH, BOARD_ID);
     }
-    
+
     @Override
     public List extractDataAsList(Image e) {
-       return Arrays.asList(e.getId(), e.getUrl(), e.getTitle(), e.getDate(), e.getLocalPath(), e.getBoard());
+        return Arrays.asList(e.getId(), e.getUrl(), e.getTitle(), e.getDate(), e.getLocalPath(), e.getBoard());
     }
 }
