df13c018e3727efa96f823d6fd3c6c228490668e
Michael Daly <daly0100@algonquinlive.com>
2020-07-01 15:08:15 -0400

    In Image entity class it expects board object not an ID, fixed it back to return the board object

--- src/main/java/logic/ImageLogic.java
+++ src/main/java/logic/ImageLogic.java
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
  * @author jonat
  */
 public class ImageLogic extends GenericLogic<Image, ImageDAL> {
-    
+
     public static final SimpleDateFormat FORMATTER = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
     public static final String ID = "id";
     public static final String URL = "url";
@@ -31,51 +31,51 @@ public class ImageLogic extends GenericLogic<Image, ImageDAL> {
     public static final String DATE = "date";
     public static final String LOCAL_PATH = "localPath";
     public static final String BOARD_ID = "boardId";
-    
+
     ImageLogic() {
         super(new ImageDAL());
-        
+
     }
-    
+
     @Override
     public List<Image> getAll() {
         return get(() -> dal().findAll());
     }
-    
+
     @Override
     public Image getWithId(int id) {
         return get(() -> dal().findById(id));
     }
-    
+
     public List<Image> getImagesWithBoardId(int boardID) {
         return get(() -> dal().findByBoardId(boardID));
     }
-    
+
     public List<Image> getImagesWithTitle(String title) {
         return get(() -> dal().findByTitle(title));
     }
-    
+
     public Image getImageWithUrl(String url) {
         return get(() -> dal().findByUrl(url));
     }
-    
+
     public Image getImageWithLocalPath(String path) {
         return get(() -> dal().findByLocalPath(path));
     }
-    
+
     public List<Image> getImagesWithDate(Date date) {
         return get(() -> dal().findByDate(date));
     }
-    
+
     public String convertDate(Date date) {
         return FORMATTER.format(date);
     }
-    
+
     @Override
     public List<Image> search(String search) {
         return get(() -> dal().findContaining(search));
     }
-    
+
     @Override
     public Image createEntity(Map<String, String[]> parameterMap) {
         Objects.requireNonNull(parameterMap, "parameterMap cannot be null");
@@ -114,10 +114,10 @@ public class ImageLogic extends GenericLogic<Image, ImageDAL> {
                 throw new ValidationException(ex);
             }
         }
-        
+
         return entity;
     }
-    
+
     public Image updateEntity(Map<String, String[]> parameterMap) {
         Objects.requireNonNull(parameterMap, "parameterMap cannot be null");
         Image entity = createEntity(parameterMap);
@@ -126,7 +126,7 @@ public class ImageLogic extends GenericLogic<Image, ImageDAL> {
         }
         return entity;
     }
-    
+
     private boolean findDuplicate(Image entity) {
         List<Image> images = getAll();
         images.remove(getWithId(entity.getId()));
@@ -137,18 +137,18 @@ public class ImageLogic extends GenericLogic<Image, ImageDAL> {
                 .collect(Collectors.toList());
         return !duplicateEntries.isEmpty();
     }
-    
+
     @Override
     public List<String> getColumnNames() {
         return Arrays.asList("ID", "URL", "Title", "Date", "LocalPath", "BoardID");
     }
-    
+
     public List<String> getColumnCodes() {
         return Arrays.asList(ID, URL, TITLE, DATE, LOCAL_PATH, BOARD_ID);
     }
-    
+
     @Override
     public List extractDataAsList(Image e) {
-        return Arrays.asList(e.getId(), e.getUrl(), e.getTitle(), e.getDate(), e.getLocalPath(), e.getBoard().getId());
+        return Arrays.asList(e.getId(), e.getUrl(), e.getTitle(), e.getDate(), e.getLocalPath(), e.getBoard());
     }
 }
